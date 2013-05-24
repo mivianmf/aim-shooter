d3824eb6e26910da1187a5ca0c1e65991232b524
pelesao@gmail.com <pelesao@gmail.com@07dcd87a-9652-d131-726b-f221c3be82fa>
2013-05-24 23:49:19 +0000

    Inicialização!

---
+++ Trabalho_Final_CG/Trabalho_Final_CG/Estruturas/Circulo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Trabalho_Final_CG.Estruturas
+{
+    class Circulo
+    {
+        //ATRIBUTOS
+        private Point centro;
+        private int raio;
+        private Color cor;
+        private Pen caneta;
+
+        public Circulo(System.Drawing.Point centro, int raio, Pen caneta, Color cor)
+        {
+            this.centro = centro;
+            this.raio = raio;
+            this.caneta = caneta;
+            this.cor = cor;
+        }
+
+        public void plotarCirculo(Graphics g, int x, int y)
+        {
+            Aplicacao.desenhar(g, caneta, centro.X + x, centro.Y + y);
+            Aplicacao.desenhar(g, caneta, centro.X - x, centro.Y + y);
+
+            Aplicacao.desenhar(g, caneta, centro.X + x, centro.Y - y);
+            Aplicacao.desenhar(g, caneta, centro.X - x, centro.Y - y);
+
+            Aplicacao.desenhar(g, caneta, centro.X + y, centro.Y + x);
+            Aplicacao.desenhar(g, caneta, centro.X - y, centro.Y + x);
+
+            Aplicacao.desenhar(g, caneta, centro.X + y, centro.Y - x);
+            Aplicacao.desenhar(g, caneta, centro.X - y, centro.Y - x);
+        }
+
+        public void draw(Graphics g)
+        {
+            //BRESENHAN
+            int x, y, p;
+            x = 0;
+            y = this.raio;
+            p = 3 - 2 * this.raio;
+
+            plotarCirculo(g, x, y);
+
+            while (x < y)
+            {
+                if (p < 0)
+                {
+                    p = p + 4 * x + 6;
+                }
+                else
+                {
+                    p = p + 4 * (x - y) + 10;
+                    y = y - 1;
+                }
+                x = x + 1;
+
+                plotarCirculo(g, x, y);
+            }
+
+        }
+    }
+}
---
+++ Trabalho_Final_CG/Trabalho_Final_CG/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_Final_CG
+{
+    using Trabalho_Final_CG.Estruturas;
+    public partial class Aplicacao : Form
+    {
+        private Pen caneta;
+        //private Circulo[] circulos;
+        private Alvo alvo;
+        private BackgroundWorker bg_thread;
+        private int quantidade_circulos;
+
+        public Aplicacao()
+        {
+            int x_aleat, y_aleat;
+
+            InitializeComponent();
+
+            //BACKGROUND COLOR
+            this.BackColor = System.Drawing.Color.White;
+
+            //BACKGROUND THREAD
+            bg_thread = new BackgroundWorker();
+
+            this.bg_thread.RunWorkerAsync();
+            bg_thread.WorkerReportsProgress = true;
+
+            //ATRIBUTOS
+            this.caneta = new Pen(System.Drawing.Color.Black, 4);
+            
+            this.alvo = new Alvo();
+                       
+            this.SetStyle(
+                System.Windows.Forms.ControlStyles.UserPaint |
+                System.Windows.Forms.ControlStyles.AllPaintingInWmPaint |
+                System.Windows.Forms.ControlStyles.OptimizedDoubleBuffer,
+                true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.UpdateStyles();
+
+            //EVENTOS
+            this.MouseMove += Aplicacao_MouseMove;
+            this.MouseClick += Aplicacao_MouseClick;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            this.alvo.draw(e.Graphics);
+        }//end OnPaint
+
+        public static void desenhar(Graphics g, Pen caneta, int x, int y)
+        {
+            g.DrawLine(caneta, new Point(x,y), new Point(x + 1, y + 1));
+        }//end desenhar
+
+        private void Aplicacao_MouseMove(object sender, MouseEventArgs args)
+        {
+            
+        }//end mouseMove
+
+        private void Aplicacao_MouseClick(object sender, MouseEventArgs args)
+        {
+
+        }//end mouseClick
+    }
+}
