1878dbce4743a5f8c8e58a6c9080f2785ed1dcd0
mivianmarques <mivianmarques@07dcd87a-9652-d131-726b-f221c3be82fa>
2013-06-02 21:11:30 +0000

    Trabalho Pronto viva Las Vegas!!

--- trunk/Trabalho_Final_CG/Trabalho_Final_CG/Form1.cs
+++ trunk/Trabalho_Final_CG/Trabalho_Final_CG/Form1.cs
@@ -25,9 +25,11 @@ namespace Trabalho_Final_CG
         private int velocidade;
         private List<Point> pontosDoDesenhoAtual;
 
-        public Aplicacao()
+        public Aplicacao(int fase, int velocidade)
         {
             InitializeComponent();
+            this.faseAtual = fase;
+            this.velocidade = velocidade;
 
             //BACKGROUND COLOR
             this.BackColor = System.Drawing.Color.White;
@@ -43,7 +45,7 @@ namespace Trabalho_Final_CG
             this.pontosDoDesenhoAtual = new List<Point>();
 
             this.pontuacao = 0L;
-            this.faseAtual = 1;
+            //this.faseAtual = 1;
 
             this.SetStyle(
                 System.Windows.Forms.ControlStyles.UserPaint |
@@ -61,18 +63,36 @@ namespace Trabalho_Final_CG
             bg_thread.DoWork += new DoWorkEventHandler(funcionaT);
 
             //TODO: IMPORTANTE
-            this.fases = new Fase[1];
+            this.fases = new Fase[3];
+            
+            //fase 1
             Desenho estrela = new Desenho(9);
             Point[] pontosEstrela = { new Point(400, 500), new Point (500,370),
-                       new Point(655,300), new Point(500,200),
-        new Point(400,55), new Point(300,200),
-        new Point (135,300), new Point(300,370),
-        new Point(400, 500)};
+                                      new Point(655,300), new Point(500,200),
+                                      new Point(400,55), new Point(300,200),
+                                      new Point (135,300), new Point(300,370),
+                                      new Point(400, 500)};
             estrela.setVertices(pontosEstrela);
-            this.fases[0] = new Fase(1, estrela, 5);
-            this.alvo = new Alvo(this.fases[0].getPontoCentral());
-            this.fases[0].adicionarObservador(this.alvo);
-            this.fases[0].adicionarObservador(this);
+            this.fases[0] = new Fase(1, estrela, this.velocidade);
+            
+
+            //fase 2 
+            Desenho estrela5pontas = new Desenho(5);
+            Point[] verticesEstrela5pontas = { new Point(190, 490), new Point(400, 125), 
+                                               new Point(585,490), new Point(160,250), 
+                                               new Point (625,250)};
+            estrela5pontas.setVertices(verticesEstrela5pontas);
+            this.fases[1] = new Fase (2, estrela5pontas,this.velocidade);
+
+            Desenho casaGrafos = new Desenho(9);
+            Point[] verticesCasa = { new Point(150, 500), new Point(675, 500), 
+                                     new Point(675,250), new Point(150,250), 
+                                     new Point (150,500), new Point (675,250),
+                                     new Point (400,75), new Point (150,250), new Point (675,500)};
+            casaGrafos.setVertices(verticesCasa);
+            this.fases[2] = new Fase(3, casaGrafos,this.velocidade);
+
+            
         }
 
         protected override void OnPaintBackground(PaintEventArgs e)
@@ -111,12 +131,33 @@ namespace Trabalho_Final_CG
 
         private void funciona(object obj, ProgressChangedEventArgs e)
         {
-            if (this.faseAtual == 1)
-            {
-                this.fases[0].iniciar();
-                this.faseAtual = -1;
-            }
 
+            switch (this.faseAtual)
+            { 
+                case 1:
+                    this.alvo = new Alvo(this.fases[0].getPontoCentral());
+                    this.fases[0].adicionarObservador(this.alvo);
+                    this.fases[0].adicionarObservador(this);
+                    this.fases[0].iniciar();
+                    this.faseAtual = -1;
+                    break;
+                
+                case 2:
+                    this.alvo = new Alvo(this.fases[1].getPontoCentral());
+                    this.fases[1].adicionarObservador(this.alvo);
+                    this.fases[1].adicionarObservador(this);
+                    this.fases[1].iniciar();
+                    this.faseAtual = -1;
+                    break;
+
+                case 3:
+                    this.alvo = new Alvo(this.fases[2].getPontoCentral());
+                    this.fases[2].adicionarObservador(this.alvo);
+                    this.fases[2].adicionarObservador(this);
+                    this.fases[2].iniciar();
+                    this.faseAtual = -1;
+                    break;
+            }
         }
 
         public void atualizar(Movimento_Subject sujeito)
